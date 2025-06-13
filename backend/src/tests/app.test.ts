58379e4245ba0cba1f0074685d7c54f2f1e3c88b
Iguana9735 <work.epilot@andreu.rocks>
2025-06-13 23:38:42 +0200

    Make a list (possibly non-exhaustive) of what the core logic must do

--- backend/src/tests/app.test.ts
+++ backend/src/tests/app.test.ts
@@ -6,4 +6,28 @@ describe("app", () => {
         const app = new App();
         expect(typeof app.getCurrentPrice()).toBe("number")
     })
+
+    // TODO
+    // Obtains the bitcoin price from an external source
+    // Caches the bitcoin price - i.e. it does not fetch it every time it is asked to do so
+    // Returns an empty list of guesses
+    // Accepts a new guess
+    // Persists the guess to an external repository
+    // Returns the guess when asked
+    // Different clients have different lists of guesses
+    // Does not accept a guess if there is a current open guess for that client
+    // Accepts a guess if there are other guesses but they are closed
+    // Resolves a guess after 60 seconds if the price has changed
+    // Resolves a guess when the price changes if the price hasn't changed after 60 seconds
+    // If after 60 seconds, the price has changed but has also returned to the original price,
+    //  then it is considered to not have changed, i.e. the guess doesn't resolve until the price changes again
+    // Test how numerical precision works. Perhaps it's better to settle on a given precision to begin with, and store
+    //  the prices as integers
+    // Resolved guesses have the time at which the guess was resolved
+    // Resolved guesses have the price at which the guess was resolved
+    // Resolved guesses are resolved as either "CORRECT" or "INCORRECT"
+    // List of guesses returns only the 5 most recent guesses
+    // Players start with a score of 0
+    // The score goes up or down as guesses are resolved
+
 })
