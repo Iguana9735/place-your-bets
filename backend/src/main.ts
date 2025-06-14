8cc8b2cde7e1762f320359f9c07c09ad695b1b6a
Iguana9735 <work.epilot@andreu.rocks>
2025-06-14 06:13:03 +0200

    Fix main setup procedure to provide a ForGettingBitcoinPrice to the app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,9 @@
 import {startServer} from "./drivingAdapters/express/server";
 import {App} from "./app/app";
+import {ForGettingBitcoinPrice} from "./drivenPorts/ForGettingBitcoinPrice";
+import FakeBitcoinPriceSource from "./drivenAdapters/FakeBitcoinPriceSource";
 
-const app = new App();
+const forGettingBitcoinPrice: ForGettingBitcoinPrice = new FakeBitcoinPriceSource()
+const app = new App(forGettingBitcoinPrice);
 
 startServer(app);
