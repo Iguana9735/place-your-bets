27273ec1f3e250af03a9098d6e49e49f4cd9cfe9
Iguana9735 <work.epilot@andreu.rocks>
2025-06-19 23:52:53 +0200

    Update guesses table to support querying a player's guesses

--- infra/src/recreate-tables.ts
+++ infra/src/recreate-tables.ts
@@ -5,8 +5,26 @@ const tableDefinitions = [{
     KeySchema: [
         { AttributeName: 'id', KeyType: 'HASH' },
     ],
+    GlobalSecondaryIndexes: [{
+        IndexName: 'playerId-submittedAt',
+        KeySchema: [
+            {
+                AttributeName: 'playerId',
+                KeyType: 'HASH',
+            },
+            {
+                AttributeName: 'submittedAt',
+                KeyType: 'RANGE',
+            },
+        ],
+        Projection: {
+            ProjectionType: 'ALL',
+        },
+    }],
     AttributeDefinitions: [
         { AttributeName: 'id', AttributeType: 'S' },
+        { AttributeName: 'playerId', AttributeType: 'S' },
+        { AttributeName: 'submittedAt', AttributeType: 'N' },
     ],
     BillingMode: 'PAY_PER_REQUEST',
 }]
