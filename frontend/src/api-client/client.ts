5cbfd2a9a4abacf233e0a28487fc50bbaa7d4e41
Iguana9735 <work.epilot@andreu.rocks>
2025-06-20 08:55:15 +0200

    Use a different ID for every player

--- frontend/src/api-client/client.ts
+++ frontend/src/api-client/client.ts
@@ -1,17 +1,28 @@
 import { Configuration, DefaultApi } from './gen'
 import type { ClientInfo } from '../model/model.ts'
+import { v4 as uuidv4 } from 'uuid'
 
 const config = new Configuration({
-    basePath: import.meta.env.VITE_BACKEND_URL
+    basePath: import.meta.env.VITE_BACKEND_URL,
 })
 const apiClient = new DefaultApi(config)
 
 const fetchClientInfo = async (): Promise<ClientInfo> => {
-    return await apiClient.getInfo({ authorization: 'player-1' })
+    return await apiClient.getInfo({ authorization: playerId() })
 }
 
 const submitNewGuess = async (direction: 'UP' | 'DOWN') => {
-    return await apiClient.submitGuess({ submitGuessRequest: { direction: direction }, authorization: 'player-1' })
+    return await apiClient.submitGuess({ submitGuessRequest: { direction: direction }, authorization: playerId() })
+}
+
+const playerId = (): string => {
+    const key = 'player-id'
+    let value = localStorage.getItem(key)
+    if (!value) {
+        value = uuidv4()
+        localStorage.setItem(key, value)
+    }
+    return value
 }
 
 export { fetchClientInfo, submitNewGuess }
